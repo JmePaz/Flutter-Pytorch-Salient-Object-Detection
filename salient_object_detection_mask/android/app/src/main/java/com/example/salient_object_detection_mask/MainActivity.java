bb32d4aad3ea809f760b81636673f9584b9b4308
James Michael E. Paz <105730089+JmePaz@users.noreply.github.com>
2023-03-26 21:08:36 +0800

    Update MainActivity.java
    Rescale input image for faster efficiency

--- salient_object_detection_mask/android/app/src/main/java/com/example/salient_object_detection_mask/MainActivity.java
+++ salient_object_detection_mask/android/app/src/main/java/com/example/salient_object_detection_mask/MainActivity.java
@@ -92,15 +92,15 @@ public class MainActivity extends FlutterActivity implements  FlutterPlugin, Met
                 Log.d("Debug", "Calling segmentation method");
                 String path = call.argument("path").toString().replace("file://", "");
                 Log.d("Debug", "Path of an Image: "+ path);
-                //final Bitmap imgBitMap = imgPathToBitmap(path);
-                Bitmap imgBitMap = null;
+                final Bitmap imgBitMap = imgPathToBitmap(path);
+               /* Bitmap imgBitMap = null;
                 try {
                    imgBitMap = BitmapFactory.decodeStream(getAssets().open("horse.jpg"));
                 }
                 catch (IOException e){
                     result.error("Error", e.getMessage(), e);
                     return;
-                }
+                }*/
                 Log.d("Debug", "Running the Model");
                // final Bitmap segmentedBitmap = imgBitMap;
                 final Bitmap segmentedBitmap = segmentObjectOnImage(imgBitMap);
